b780e23068ece25bc2023b9e6290e765735bbe2f
codemonster00 <stephenmanukure@gmail.com>
2022-07-14 21:17:09 +0800

    Add project files.

---
+++ Models/Accounts/SignUpModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignUpModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
